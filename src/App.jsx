745e97bcc2441a3320d63b859b173cb401375cdd
versario <pablogaticastuart@gmail.com>
2026-01-01 14:44:52 -0300

    chore: migrate toast notifications to sonner

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import './App.css'
-import { Toaster } from "@/components/ui/toaster"
+import { Toaster } from 'sonner';
 import { QueryClientProvider } from '@tanstack/react-query'
 import { queryClientInstance } from '@/lib/query-client'
 import VisualEditAgent from '@/lib/VisualEditAgent'
